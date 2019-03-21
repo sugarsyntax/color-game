d60f4ef4c5d2f2e30941b5533bfaad0607c98ea0
Jacquelline Beltran <jacquelline500@gmail.com>
2019-03-21 14:10:57 -0700

    Test random colors on squares

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 // stores number of squares available per game
 var numSquares = 6;
 // empty array contains random six or three times every new game
-var colors = [];
+var colors = [
+  "rgb(255, 0, 0)",
+  "rgb(255, 0, 255)",
+  "rgb(255, 225, 0)",
+  "rgb(255, 0, 255)",
+  "rgb(0, 255, 255)",
+  "rgb(0, 255, 0)"
+];
 // color selected by user
 var pickedColor;
 // array of all squares available on page depending on options
@@ -10,3 +17,7 @@ var squares = document.querySelectorAll(".square");
 var resetButton = document.querySelector("#reset");
 // array that contains "easy" and "hard" mod buttons
 var modeButtons = document.querySelectorAll(".mode");
+
+for (let i = 0; i < squares.length; i++) {
+  squares[i].style.backgroundColor = colors[i];
+}
