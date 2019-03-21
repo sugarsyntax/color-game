5c3fcbda111d242e57ddfbe9135f64a845cb7907
Jacquelline Beltran <jacquelline500@gmail.com>
2019-03-20 23:33:01 -0700

    Add comments to variables

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
+// stores number of squares available per game
 var numSquares = 6;
+// empty array contains random six or three times every new game
 var colors = [];
+// color selected by user
 var pickedColor;
+// array of all squares available on page depending on options
 var squares = document.querySelectorAll(".square");
+// "new game" button
 var resetButton = document.querySelector("#reset");
+// array that contains "easy" and "hard" mod buttons
 var modeButtons = document.querySelectorAll(".mode");
